f84e11c0da6b9530967d53baa828f40bf5936831
Jochem Crab <jochem.crab@student.ehb.be>
2021-01-13 00:18:03 +0100

    Some random ass requests

---
+++ app/src/main/java/com/example/movietinder/Main.java
@@ -0,0 +1,19 @@
+package com.example.movietinder;
+
+import
+
+public class Main {
+    public static void main(String[] args){
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://imdb8.p.rapidapi.com/title/auto-complete?q=game%20of%20thr"))
+                .header("x-rapidapi-key", "38fa1df955mshca2148d13a7b88ap1eba29jsn0f0eb8a3af8f")
+                .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+    public void doSomething(){
+
+    }
+}
---
+++ app/src/main/java/com/example/movietinder/Movie.java
@@ -0,0 +1,7 @@
+package com.example.movietinder;
+
+public class Movie {
+    private String title;
+    private String description;
+    private String shortDescription;
+}
