ada50c22d469d9355b4ab190a4fa40d0511e5621
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 01:31:59 +0100

    Fixed Java version Changed API to OkHttp

--- app/src/main/java/com/example/movietinder/Main.java
+++ app/src/main/java/com/example/movietinder/Main.java
@@ -1,17 +1,19 @@
 package com.example.movietinder;
 
-import java.net.http.*;
+import 
 
 public class Main {
     public static void main(String[] args){
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://imdb8.p.rapidapi.com/title/auto-complete?q=game%20of%20thr"))
-                .header("x-rapidapi-key", "38fa1df955mshca2148d13a7b88ap1eba29jsn0f0eb8a3af8f")
-                .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://imdb8.p.rapidapi.com/title/auto-complete?q=game%20of%20thr")
+                .get()
+                .addHeader("x-rapidapi-key", "ad20faac89msh7f16d75b1a51be9p1709dajsnba93ea5ea2b7")
+                .addHeader("x-rapidapi-host", "imdb8.p.rapidapi.com")
                 .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        Response response = client.newCall(request).execute();
     }
     public void doSomething(){
 
