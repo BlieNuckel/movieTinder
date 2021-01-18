02b5d449a7ba3f117c3a02e86c640e28aa471f95
Lasse Poulsen <70920150+BlieNuckel@users.noreply.github.com>
2021-01-18 20:34:49 +0100

    Delete app directory

--- app/src/main/java/com/example/movietinder/Friend.java
+++
@@ -1,9 +0,0 @@
-package com.example.movietinder;
-
-import java.util.ArrayList;
-
-public class Friend {
-    String username;
-    String userId;
-    ArrayList<Movie> likedMovies = new ArrayList<>();
-}
--- app/src/main/java/com/example/movietinder/Movie.java
+++
@@ -1,8 +0,0 @@
-package com.example.movietinder;
-
-public class Movie {
-    private String coverUrl;
-    private String title;
-    private String description;
-    private String shortDescription;
-}
