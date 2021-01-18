42ba950f77e0932500557d2977eac73cde7bc11a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 03:40:00 +0100

    Created Friend class

---
+++ app/src/main/java/com/example/movietinder/Friend.java
@@ -0,0 +1,9 @@
+package com.example.movietinder;
+
+import java.util.ArrayList;
+
+public class Friend {
+    String username;
+    String userId;
+    ArrayList<Movie> likedMovies = new ArrayList<>();
+}
--- app/src/main/java/com/example/movietinder/Movie.java
+++ app/src/main/java/com/example/movietinder/Movie.java
@@ -1,6 +1,7 @@
 package com.example.movietinder;
 
 public class Movie {
+    private String coverUrl;
     private String title;
     private String description;
     private String shortDescription;
