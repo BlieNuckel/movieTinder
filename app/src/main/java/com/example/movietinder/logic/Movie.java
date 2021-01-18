1c32a864da06cf2d3ae9d74b703648be34e6ef63
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 20:40:23 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/movietinder/logic/Movie.java
@@ -0,0 +1,8 @@
+package com.example.movietinder.logic;
+
+public class Movie {
+    private String coverUrl;
+    private String title;
+    private String description;
+    private String shortDescription;
+}
